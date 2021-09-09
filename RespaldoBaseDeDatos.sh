105ca0bf40b42a53cd38a3a3651ba903f8fe6f52
Ignacio Aguiar <86125314+Hitatsu@users.noreply.github.com>
2021-09-09 20:00:37 -0300

    Arreglado Comparador

--- RespaldoBaseDeDatos.sh
+++ RespaldoBaseDeDatos.sh
@@ -3,3 +3,8 @@ mysqldump -u root –p miclave3035
 mibase > mibase.sql 
 gzip mibase.sql
 mv mibase.sql.gz /bkp/bkp-Mysql
+
+count=`ls /bkp/bkp-Mysql | wc -l`
+if [ "$count" -gt "3" ] then
+
+	rm "$(ls -t | tail -1)"
--- SambaBackup.sh
+++ SambaBackup.sh
@@ -2,3 +2,8 @@
 cp /samba /sambaBackup
 gzip /sambaBackup 
 mv /sambaBackup.gz /bkp/bkp-samba
+
+count=`ls /bkp/bkp-samba | wc -l`
+if [ "$count" -gt "3" ] then
+
+	rm "$(ls -t | tail -1)"
